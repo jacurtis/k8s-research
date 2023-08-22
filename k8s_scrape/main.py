a5e643b898c4b14aaeb540c65d459972ae8f1ecf
Alex Curtis <jacurtis@hey.com>
2023-08-21 21:44:17 -0600

    Update kubernetes csvs, replacing 1k numbers with 1,000 or equivilant

--- k8s_scrape/main.py
+++ k8s_scrape/main.py
@@ -7,12 +7,12 @@ import stackoverflow
 def main():
     stackoverflow.scrape_so_index_page(
         tag="kubernetes",
-        filename="../datasources/kubernetes-10.csv",
-        pages=100,
-        page_start=700,
+        filename="../datasources/kubernetes-11.csv",
+        pages=50,
+        page_start=1,
         page_size=50
     )
 
 
 if __name__ == '__main__':
-    main()
+    main()  # You might want to run `poetry update` first, to get the latest chromedriver
